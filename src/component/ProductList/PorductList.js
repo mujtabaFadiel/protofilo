41eac7e423b407a89647be6fbae042ece7f2709c
mujtabaFadiel <mujtabafadiel@gmail.com>
2026-04-30 10:52:53 +0400

    update

--- src/component/ProductList/PorductList.js
+++ src/component/ProductList/PorductList.js
@@ -3,11 +3,13 @@ import './porductList.css';
 import project1 from '../../images/project1.PNG'
 import project2 from '../../images/project2.PNG'
 // استيراد صور المشاريع الجديدة (تأكد من الأسماء الصحيحة للصور)
-import chatImg from '../../images/chat-app.jpeg' 
+import chatImg from '../../images/chat-app.jpeg'
+import chatapp from '../../images/chatapp.jpg'
 import chatDbImg from '../../images/chatDbImg.png'
+import videoDemo from '../../images/videoDemo.mp4'
 
 function PorductList() {
-    return(
+    return (
         <div className="pl">
             <div className='pl-texts'>
                 <h1 className='pl-title'>Create & Inspire</h1>
@@ -16,41 +18,52 @@ function PorductList() {
                 </p>
             </div>
             <div className='pl-list'>
-                {/* مشروع الـ Social App الجديد */}
-                <Product 
-                  title="Wave Social App"
-                  p="Register & Login (JWT Auth)
-Create posts
-Like & Comment
-Follow users
-View feed (following users only)"
-                  link="https://github.com/mujtabaFadiel/Wave-App" // رابط المستودع
-                  img={chatImg}
-                  imgMobile
+                <Product
+                    title="Wave Social App"
+                    p="Register & Login (JWT Auth)
+                    Create posts
+                    Like & Comment
+                    Follow users
+                    View feed (following users only)"
+                    link="https://github.com/mujtabaFadiel/Wave-App" // رابط المستودع
+                    img={chatImg}
+                    imgMobile
+                />
+
+                <Product
+                    title="ChatApp FrontEnd"
+                    p="Developed a high-performance, real-time messaging application 
+                    using NestJS for the backend and React Native for the mobile interface.
+                    "
+                    link="https://github.com/mujtabaFadiel/chat-app-frontend" // رابط المستودع
+                    img={chatapp}
+                    video
+                    videoSrc={videoDemo}
+                    imgVideo
                 />
 
                 {/* مشروع الـ Chat App الجديد */}
-                <Product 
-                  title="Real-Time Chat Backend"
-                  link="https://github.com/mujtabaFadiel/chat-app-backend" 
-                  img={chatDbImg}
-                  p="Real-Time Chat Backend
+                <Product
+                    title="ChatApp Backend"
+                    link="https://github.com/mujtabaFadiel/chat-app-backend"
+                    img={chatDbImg}
+                    p="Real-Time Chat Backend
 
-- Built with NestJS and WebSocket (Socket.IO)
-- JWT authentication and PostgreSQL database
-- Private messaging using rooms"
+                    - Built with NestJS and WebSocket (Socket.IO)
+                    - JWT authentication and PostgreSQL database
+                    - Private messaging using rooms"
                 />
 
                 {/* مشاريعك السابقة */}
-                <Product 
-                  title="landing page"
-                  link="https://mujtabafadiel.github.io/react-js/" 
-                  img={project1}
+                <Product
+                    title="landing page"
+                    link="https://mujtabafadiel.github.io/react-js/"
+                    img={project1}
                 />
-                <Product 
-                  title="ebook landingPage"
-                  link="https://mujtabafadiel.github.io/ebook-Templete/" 
-                  img={project2}  
+                <Product
+                    title="ebook landingPage"
+                    link="https://mujtabafadiel.github.io/ebook-Templete/"
+                    img={project2}
                 />
             </div>
         </div>
--- src/component/porduct/Porduct.js
+++ src/component/porduct/Porduct.js
@@ -1,7 +1,7 @@
 import './porduct.css';
 
 const Product = (props) => {
-    return(
+    return (
         <div className="p">
             <div className=''>
                 <div className='p-circle'></div>
@@ -9,11 +9,21 @@ const Product = (props) => {
                 <div className='p-circle'></div>
             </div>
             <a href={props.link} target="_blank" rel='noreferrer'>
-                <img 
-                    src={props.img} 
-                    alt="pic" 
-                    className={props.imgMobile ? 'img-mobile' : 'img'}
-                />
+                {
+                    !props.video ? (<img
+                        src={props.img}
+                        alt="pic"
+                        className={props.imgMobile ? 'img-mobile' : 'img'}
+                    />) : (<video
+                        src={props.videoSrc}
+                        className={'img-video'}
+                        autoPlay
+                        muted
+                        loop
+                        playsInline
+                        poster={props.img} // صورة تظهر قبل تحميل الفيديو
+                    />)
+                }
             </a>
             <h3>{props.title}</h3>
             <p className='pl-desc'>{props.p}</p>
